f72c72ca4977fe82bac23928a558aac133891701
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-06-15 21:35:39 +0900

    ABC358

---
+++ ABC/ABC358/ABC358_A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+
+#define rep(i, n) for(int i=1; i<=n; i++)
+using namespace std;
+
+int main(){
+    string S,T;
+    cin >> S >> T;
+    if(S=="AtCoder"&&T=="Land") cout << "Yes";
+    else cout << "No";
+
+    return 0;
+}
