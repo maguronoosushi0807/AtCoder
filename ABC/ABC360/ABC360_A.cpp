d9d501166ddefc6e569f5444dadc3c03cd2abe2c
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-07-03 23:59:00 +0900

    ABC360
    頭が回らない

--- ABC/ABC360/ABC360_A.cpp
+++ ABC/ABC360/ABC360_A.cpp
@@ -7,23 +7,10 @@ using namespace std;
 #define rep(i,n) for(int i=1;i<=n;i++)
 
 int main(){
-    int N; cin >> N;
     string S; cin >> S;
 
-    int ans = 0;
-    for(int i=0;i<=N-3;i++){
-        // cout << i << "-" << i+3 << ":" << S.substr(i,3) << " ";
-        cout << ans << " ";
-        if(S.substr(i,3)=="ABA"){
-            // ans = (ans+1)%1000000007;
-            ans++;
-        }else if(S.substr(i,3)=="BAB"){
-            // ans = (ans+1)%1000000007;
-            ans++;
-        }
-    }
-
-    cout << ans;
+    if(S[0]=='R' || S=="SRM") cout << "Yes";
+    else cout << "No";
 
     return 0;
 }
